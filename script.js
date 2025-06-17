33d137c9768e7cfb0f3f418854bb8081a16ac657
Animes <animesbarman101@gmail.com>
2025-06-17 15:41:08 +0600

    okk

--- script.js
+++ script.js
@@ -43,9 +43,100 @@
 
 //16-7 Introduction to arithmetic operators
 
-var x=90;
-var y=100;
+// var x=90;
+// var y=100;
 
-var total=x+y;
+// var total=x+y;
 
-console.log(total);
+// console.log(total);
+
+
+// var u=10;
+// var k=3;
+
+// console.log(u/k);
+// console.log(u%k);
+
+
+// console.log(u=u+70);
+
+// console.log(u +=40);
+// console.log(u -=40);
+// console.log(u *=40);
+// console.log(u /=40);
+// console.log(u +='44');
+
+
+// 17_1 Compare variables and Comparison operato
+
+//Biger: >
+//less: <
+//equal: ==
+//>=
+//<=
+//!=  and &&  or || 
+
+
+// const  apple=90;
+// const ornage=50;
+
+// console.log(apple > ornage)
+
+// console.log(ornage <apple)
+
+// console.log(apple == ornage);
+
+// console.log(apple != ornage);
+
+// console.log(apple === ornage)
+
+// 17_3 Introduction to if Else Condition
+
+// if(50>20){
+//     console.log('ok')
+// }else{
+//     console.log('no')
+// }
+// 17_4 Multiple condition Logical Operators
+
+
+
+
+// if(40>30 && 30<90){
+//     console.log('ok')
+// }else{
+//     console.log('no')
+// }
+
+
+// const tshirtPrices=500000;
+
+// if(tshirtPrices > 50000 ){
+//     // discount
+
+//     const discount=tshirtPrices * 10 /100;
+//     const payAmount=tshirtPrices -discount;
+
+//     console.log(payAmount)
+// }else{
+//     console.log(tshirtPrices)
+// }
+
+// 17_7 (advanced )If-else shorthand Ternary Operator
+
+
+// console.log(400>90 ? 'okk' : 'noo')
+
+// 17_8 (advanced) Logical Not Operator
+
+// const isLoading=false;
+
+// if(!isLoading){
+
+//     console.log('okkk')
+// }else{
+//     console.log('nooo')
+// }
+
+
+

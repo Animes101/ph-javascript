67483d79a8eb342baadd347fc6649c1243ffe594
Animes <animesbarman101@gmail.com>
2025-06-17 12:28:06 +0600

    árit matick

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
-var name='animes barman';
+//var
 
-console.log(name)
+// var weight=90;
+
+// console.log(weight);
+
+// var places='thakurgaon';
+
+// console.log(places);
+
+//16-4 Variable data types, Numeric, String, Boolean
+
+// var numbers=80;
+// var stringss='stringesss';
+// var bool=true;
+
+// console.log(typeof(numbers));
+// console.log(typeof(stringss));
+// console.log(typeof(bool))
+
+
+//16-5 JavaScript Keyword, Variable name naming convention
+
+
+//camelase case use
+
+//16-6 Know the fundamentals of JS Numbers
+
+
+// var chokolat=4.50;
+// var mango=80;
+// var ornage='33'
+
+// var total=mango+ parseInt(chokolat);
+
+// console.log(total);
+
+
+// var numbersTofixed=5.5059599594490;
+
+// console.log(numbersTofixed.toFixed(5))
+
+
+//16-7 Introduction to arithmetic operators
+
+var x=90;
+var y=100;
+
+var total=x+y;
+
+console.log(total);
